c0429f614e36278bd1d8ee75ef841c7f2775fb49
pontsuyo4423 <tsuyopi4423@gmail.com>
2024-01-26 18:20:01 +0900

    カタカナ入力のみを許可

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  KATAKANA_REGEXP = /\A[\p{katakana}\u{30fc}]+\z/
   validates :nickname, presence: true
   validates :last_name, presence: true
   validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :last_name_kana, presence: true, format: { with: KATAKANA_REGEXP}
+  validates :first_name_kana, presence: true, format: { with: KATAKANA_REGEXP}
   validates :birthday, presence: true
+  validates :password, format: {
+    with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+  },on: :create
 end
