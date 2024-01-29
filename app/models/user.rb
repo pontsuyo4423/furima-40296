0259a4ecbfcba6316c9bd280a18ce99259725635
pontsuyo4423 <tsuyopi4423@gmail.com>
2024-01-29 09:55:10 +0900

    全角入力のみを許可する

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   KATAKANA_REGEXP = /\A[\p{katakana}\u{30fc}]+\z/
+  ZENKAKU = /\A[ぁ-んァ-ン一-龥]/
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
+  validates :last_name, presence: true, format: {with: ZENKAKU}
+  validates :first_name, presence: true, format: {with: ZENKAKU}
   validates :last_name_kana, presence: true, format: { with: KATAKANA_REGEXP}
   validates :first_name_kana, presence: true, format: { with: KATAKANA_REGEXP}
   validates :birthday, presence: true
