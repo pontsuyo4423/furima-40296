12bacf1116059cdab467b6338a4b6efb8e15efa1
pontsuyo4423 <tsuyopi4423@gmail.com>
2024-01-30 12:22:15 +0900

    写真を必須にする

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,6 +20,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name,:description,:category_id,:item_status_id,:shipping_charge_id,:prefecture_id,:shipping_date_id,:price)
+    params.require(:item).permit(:name,:description,:category_id,:item_status_id,:shipping_charge_id,:prefecture_id,:shipping_date_id,:price,:image)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -6,12 +6,14 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shipping_date
   belongs_to :user
+  has_one_attached :image
 
-  validates :name, :description, :price, presence: true
+  validates :name, :description, :image, presence: true
 
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :item_status_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :shipping_charge_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :shipping_date_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999},presence: {message: "can't be blank"}
 end
