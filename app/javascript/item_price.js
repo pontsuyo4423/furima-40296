6f1a7fb6f59f606edc6a76c36d9c94376265f147
pontsuyo4423 <tsuyopi4423@gmail.com>
2024-01-30 15:13:11 +0900

    手数料がリアルタイムに反映

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,10 +1,9 @@
-window.addEventListener('load', () => {
+window.addEventListener('turbo:load', () => {
   const priceInput = document.getElementById("item-price");
-  priceInput.addEventListener("input", () => {
-  const inputValue = priceInput.value;
-  const addTaxDom = document.getElementById("add-tax-price");
-  const ProfitDom = document.getElementById("profit");
-  addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
-  ProfitDom.innerHTML = inputValue - addTaxDom.innerHTML;
-  })
+   priceInput.addEventListener("input", () => {
+     const addTaxDom = document.getElementById("add-tax-price");   
+     addTaxDom.innerHTML = Math.round(priceInput.value * 0.1 );
+     const addPofitDom = document.getElementById("profit");
+     addPofitDom.innerHTML = Math.round(priceInput.value - Math.round(priceInput.value * 0.1 ))
+ })
 });
