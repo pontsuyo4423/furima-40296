23c560fb107185eca62f374a879bc5c2c67c999f
pontsuyo4423 <tsuyopi4423@gmail.com>
2024-02-02 18:18:24 +0900

    購入内容を表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,12 @@
 class OrdersController < ApplicationController
-  def create
+  before_action :set_item, only: [:index]
+
+  def index
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 end
