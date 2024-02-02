b3b8532efb67db2444948802af049c5c47af1ed5
pontsuyo4423 <tsuyopi4423@gmail.com>
2024-02-02 17:59:13 +0900

    ordersモデル、コントローラーの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,6 +6,7 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shipping_date
   belongs_to :user
+  has_one :order
   has_one_attached :image
 
   validates :name, :description, :image, presence: true
