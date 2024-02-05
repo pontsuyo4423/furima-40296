bd394b12be72cc912c08f353a62114bd42a45b5f
pontsuyo4423 <tsuyopi4423@gmail.com>
2024-02-05 11:12:55 +0900

    hogeを適切なものに変更

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,25 @@
 class OrdersController < ApplicationController
-  before_action :set_item, only: [:index]
+  before_action :set_item, only: [:index, :create]
 
   def index
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 
   private
 
   def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def order_params
+    params.require(:order).permit(:postcode, :city, :block, :building, :phone_number).merge(item_id: @item.id)
   end
 end
